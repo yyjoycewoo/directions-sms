fa40155ee115fef8b3a3b72130929ab996e4ec48
Ali Raza <alir.raza@mail.utoronto.ca>
2019-01-19 21:45:34 -0500

    backend for twilio pushed

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from html.parser import HTMLParser
-import requests, os
+import requests, os, asyncio
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
@@ -29,44 +29,44 @@ def sms_ahoy_reply():
     resp = MessagingResponse()
 
     # Add a message
-    destination = resp.message(request.args.get('Body'))
-
+    body = request.args.get('Body')
+    body=body.split()
+    longLat = body[-1]
+    dest=body[3:len(body)-2]
+    destination="+".join(dest)
+    resp.message(getRespfromGoogle(origin=longLat,destination=destination))
     return str(resp)
 
-@app.route("/directions", methods=['GET'])
 def getRespfromGoogle(origin = "bahen+uoft", destination = "hart+house", travelType = "walking"):
     #Using Google maps API to retreive directions from origin to destination
     map = "" #our string to request from Google maps
     map += "https://maps.googleapis.com/maps/api/directions/json?origin="+origin
     map += "&destination=" + destination
-    map += "&mode=" + travelType + gKey
+    map += "&mode=" + travelType
+    map += "&key="+gKey
     req = requests.get(map)
 
     if req.status_code!=200: #if response was unsucessful
-        return "error"
+        return ["error"]
 
-    directionsJson = (req.json())
+    directionsJson = req.json()
 
     for i in directionsJson['geocoded_waypoints']: #check if valid origin and destination
         if i['geocoder_status']!="OK":
-            return "direction error"
+            return ["direction error"]
     # directions now gives us a JSON of directions to reach the dest.
     directions = directionsJson['routes'][0]['legs'][0]
-    statement = "From your location, you are " + directions['distance']['text'] + " away."
-    statement+= "\n It will take "+directions['duration']['text'] + " to reach " + directions['end_address']
-    count = 1
-    steps = [] #steps is a list of directions to reach dest.
+    statement = "\nFrom your location, you are " + directions['distance']['text'] + ", away.\n"
+    statement+= "\n It will take "+directions['duration']['text'] + ", to reach " + directions['end_address']+".\n\n"
+    step_count = 1
+    steps = statement #steps is a list of directions to reach dest.
+
     for i in directions['steps']:
-        step = "Step " + str(count) + ":" + "\n"
-        step+= "For" + i['distance']['text']
+        step = "Step " + str(step_count) + ": "
         instructions = strip_tags(i['html_instructions'])
-        step+= instructions + " \n"
-        step+= "For " +i['duration']['text']
-        steps.append(step)
+        step+= instructions + " "
+        step+= "for "+i['distance']['text'] + " which will take " +i['duration']['text'] + "\n";
+        step_count+=1
+        steps+= step
 
-
-
-
-
-
-getRespfromGoogle()
+    return steps
